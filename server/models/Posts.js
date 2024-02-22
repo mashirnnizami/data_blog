ba630856f5ebcf84dd7d908be9be27cb1d215209
Mashir Nizami <34296921+mashirnnizami@users.noreply.github.com>
2024-02-22 21:20:08 +0530

    Add files via upload

---
+++ server/models/Posts.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Datatypes) => {
+  const Posts = sequelize.define("Posts", {
+    title: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    postText: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Posts;
+};
